451f7ec57e0ea007b56e1956bc83d8bce496db6f
Ryan Tanay <ryantanay@newevolution.org>
2016-02-18 11:21:59 -0600

    Added view note functionality and changed redirect from new note to view that note

--- server.js
+++ server.js
@@ -35,11 +35,17 @@ const Note = mongoose.model('notes', mongoose.Schema({
   text: String
 }));
 
+app.get('/notes/:id', (req, res) => {
+  Note.findById(req.params.id, (err, note) => {
+    if (err) throw err;
+    res.render('show-note', {note: note})
+  });
+});
+
 app.post('/notes', (req, res) => {
   Note.create(req.body, (err, note) => {
     if (err) throw err;
-    console.log(note); // eslint-disable-line no-console
-    res.redirect('/');
+    res.redirect(`/notes/${note._id}`);
   });
 });
 
