c18aba5a97cb5885824de89582534b4dff1e416b
Ryan Tanay <ryantanay@newevolution.org>
2016-02-18 10:05:37 -0600

    Added basic node server

---
+++ server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const express = require('express');
+const favicon = require('favicon'); // eslint-disable-line no-unused-vars
+const mongoose = require('mongoose'); // eslint-disable-line no-unused-vars
+const bodyParser = require('body-parser'); // eslint-disable-line no-unused-vars
+const localPort = 3000;
+const port = process.env.port || localPort;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('<h1>Yo</h1>');
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`); // eslint-disable-line no-console
+});
