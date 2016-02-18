a493c6364d0835963baee08f664d5f95c0fdc398
Ryan Tanay <ryantanay@newevolution.org>
2016-02-18 10:35:47 -0600

    Added new note submission form and handler, included Jade for templating & body-parser for form data parsing

--- server.js
+++ server.js
@@ -3,11 +3,31 @@
 const express = require('express');
 const favicon = require('favicon'); // eslint-disable-line no-unused-vars
 const mongoose = require('mongoose'); // eslint-disable-line no-unused-vars
-const bodyParser = require('body-parser'); // eslint-disable-line no-unused-vars
+const bodyParser = require('body-parser');
+
+const date = new Date();
 const localPort = 3000;
 const port = process.env.port || localPort;
 
 const app = express();
+
+app.locals.title = "Evernode";
+app.locals.year = date.getFullYear();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.set('view engine', 'jade');
+
+app.get('/notes/new', (req, res) => {
+  res.render('new-note');
+});
+
+app.post('/notes', (req, res) => {
+  const body = req.body;
+  console.log('body', body); // eslint-disable-line no-console
+  res.redirect('/');
+});
 
 app.get('/', (req, res) => {
   res.send('<h1>Yo</h1>');
