5708d5c5c60e5a29f186b5d776b056b1d03a8e29
Ryan Tanay <ryantanay@newevolution.org>
2016-02-18 11:00:21 -0600

    Missing |

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || localPort;
 const MONGO_PORT = process.env.MONGO_PORT || localMongoPort;
 const MONGO_HOST = process.env.MONGO_HOST || 'localhost';
 const MONGO_USER = process.env.MONGO_USER || '';
-const MONGO_PASS = process.env.MONGO_PASS | '';
+const MONGO_PASS = process.env.MONGO_PASS || '';
 const MONGO_AUTH = MONGO_USER ? `${MONGO_USER}:${MONGO_PASS}@` : '';
 const MONGO_URL = `mongodb://${MONGO_AUTH}${MONGO_HOST}:${MONGO_PORT}/evernode`;
 
