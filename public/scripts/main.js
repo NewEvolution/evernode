fb74aa1be2ba71d8f4a9a78385f766e97156224c
Ryan Tanay <ryantanay@newevolution.org>
2016-03-09 15:41:32 -0600

    Working on checklists, styling is all blown up atm

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,12 @@
 /* eslint strict: 0, no-unused-vars: 0, prefer-arrow-callback: 0 */
-$('#logout').click(function logout () {
+$('#logout').click(function () {
   $.post('/login?_method=delete');
+});
+
+$('body').on('click', '.checkbox', function () {
+  if (this.checked) {
+    $(this).next('.checkline').addClass('struck');
+  } else {
+    $(this).next('.checkline').removeClass('struck');
+  }
 });
